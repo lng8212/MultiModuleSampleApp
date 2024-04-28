d55c7f79c5911c401666a602629660b79f554d1c
Larry <long05012001@gmail.com>
2024-04-28 21:41:03 +0700

    optimize

--- feature/message/src/main/java/com/longkd/feature/message/ui/MessageScreen.kt
+++ feature/message/src/main/java/com/longkd/feature/message/ui/MessageScreen.kt
@@ -1,15 +1,7 @@
 package com.longkd.feature.message.ui
 
-import android.util.Log
-import android.widget.EditText
-import android.widget.Space
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyColumn
