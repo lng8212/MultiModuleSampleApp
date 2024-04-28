b5197ebb7d149d602b0cdfaa22438a2830fb630b
Larry <long05012001@gmail.com>
2024-04-28 15:21:56 +0700

    initial

---
+++ app/src/main/java/com/longkd/multimodulesampleapp/Application.kt
@@ -0,0 +1,12 @@
+package com.longkd.multimodulesampleapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @Author: longkd
+ * @Since: 14:58 - 28/04/2024
+ */
+@HiltAndroidApp
+class Application: Application() {
+}
